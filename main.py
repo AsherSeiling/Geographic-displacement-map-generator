c0a4b99ef70722be07338ec7bbc3034036d470d4
AsherSeiling <f14asher@gmail.com>
2020-11-30 14:08:10 -0600

    Height data, and start on reading data.

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+import PIL
+from PIL import Image
+
+# Reads data from TXT file
+my_file = open('Exaple_terrain_height_data.txt')
+read_lines = my_file.readlines()
+lines_str = []
+for i in read_lines:
+    lines_str.append(i)
+read_without_n = x[:-1] for x in items]
+
+print(new_items)
